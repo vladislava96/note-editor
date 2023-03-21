91ca6a77199ed5a88e8d4fede19ddec5ad1bc272
ULADZISLAVA <vladislava96.96@mail.ru>
2023-03-21 14:20:20 +0300

    feat: implement saving notes to localStorage

--- src/features/notes/notesSlice.ts
+++ src/features/notes/notesSlice.ts
@@ -1,4 +1,4 @@
-import { createEntityAdapter, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createEntityAdapter, createSlice, PayloadAction, Update } from "@reduxjs/toolkit";
 
 export type Note = { id: number; text: string, tags: string[] };
 
@@ -10,16 +10,40 @@ export const notesSelectors = notesAdapter.getSelectors();
 
 let id = 1;
 
+const initialState = notesAdapter.getInitialState(JSON.parse(localStorage.getItem('note') ?? ''))
+
+const notes = notesSelectors.selectAll(initialState)
+
+for (const note of notes) {
+  if (note.id + 1 > id) {
+    id = note.id + 1
+  }
+}
+
+type InitialState = typeof initialState;
+
+function saveToLocalStorage(state: InitialState) {
+  let json = JSON.stringify(state);
+  localStorage.setItem('note', json)
+}
+
 const notesSlice = createSlice({
   name: 'notes',
-  initialState: notesAdapter.getInitialState(),
+  initialState,
   reducers: {
     addNote(state, action: PayloadAction<Note>) {
       action.payload.id = id++;
       notesAdapter.addOne(state, action);
+      saveToLocalStorage(state);
     },
-    updateNote: notesAdapter.updateOne,
-    removeNote: notesAdapter.removeOne
+    updateNote(state, action: PayloadAction<Update<Note>>) {
+      notesAdapter.updateOne(state, action);
+      saveToLocalStorage(state);
+    },
+    removeNote(state, action: PayloadAction<number>) {
+      notesAdapter.removeOne(state, action);
+      saveToLocalStorage(state);
+    } 
   },
 })
 
