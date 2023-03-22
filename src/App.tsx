8332849ce0237c81fae6de963f5a295a61dbfae9
ULADZISLAVA <vladislava96.96@mail.ru>
2023-03-22 19:03:10 +0300

    feat: implement export notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useAppDispatch } from "./app/hooks";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { selectNoteForEdit } from "./features/note-editor/NodeEditorSlice";
 import NoteEditor from "./features/note-editor/NoteEditor";
 import NoteList from "./features/note-list/NoteList";
+import { notesSelectors } from "./features/notes/notesSlice";
 import './index.scss';
 
 function App() {
   const dispatch = useAppDispatch();
 
+  let notes = useAppSelector(state => notesSelectors.selectAll(state.notes));
+  let dataHref = "data:json;charset=utf-8," + encodeURIComponent(JSON.stringify(notes));
+
   function onCreateButtonClick() {
-    dispatch(selectNoteForEdit({ 
+    dispatch(selectNoteForEdit({
       id: 0,
       text: '',
       tags: []
@@ -19,11 +23,12 @@ function App() {
     <div className="App">
       <div className="App__block">
         <button
-          className="btn btn-primary" 
+          className="btn btn-primary"
           onClick={onCreateButtonClick}
         >
           Create
         </button>
+        <a style={{marginLeft: "10px"}} href={dataHref} download="notes.json">Export file</a>
         <NoteEditor />
       </div>
       <NoteList />
